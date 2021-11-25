f2771ed6861d104eab48a68b8a8b35a6f4360d84
nurmatovabdulbosit <boysa1993@gmail.com>
2021-11-25 09:16:19 +0500

    yangi fayl qoshdim urganish uchun
    pythonni qoshdim

---
+++ python/payhton.py
@@ -0,0 +1,33 @@
+
+#print("salom")
+#print('Salom "men" sanga')
+#print("salom \"dell\" komp sotib oldim")
+#print('qo\'l')
+# / onlik bulish
+# // birlik bulish
+# ** kvadrat chiqarish
+
+#ism="abdulbosit"
+#yosh=28
+
+#print(ism)
+#print(yosh)
+
+#mehmonlar = ['ali','vali','dali','otash']
+#for i in mehmonlar:
+#    print(f"Salom {i}, sizni toyga")
+#    print("Hayr", i)
+
+#sonlar = list(range(11))
+#sonlar_kv = []
+#for i in sonlar:
+    #print(f"{i} ning kavdraati {i**2} ga teng")
+ #   sonlar_kv.append(i**2)
+#print(sonlar)
+#print(sonlar_kv)
+#dos = []
+#print("5 ta ism kiriting")
+#for n in range(5):
+#    dos.append(input(f"{n} - imsni kirit: "))
+#print(dos)
+    
